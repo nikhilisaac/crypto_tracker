9c9980c2b0589b744f094c0d0ae5573c0233e806
nikhil <nikhil.isaac20@pccoepune.org>
2021-10-06 19:24:21 +0530

    Initial commit

---
+++ main.py
@@ -0,0 +1,11 @@
+import numpy as np
+import pandas as pn
+import streamlit as st
+from PIL import image
+import base64
+import matplotlib.pyplot as plt
+from bs4 import BeautifulSoup
+import json
+import time
+
+
